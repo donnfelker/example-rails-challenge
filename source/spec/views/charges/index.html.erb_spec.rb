88811d3b2b7b4dfcebfe102f7215603c9a726764
Jose Corcuera <jzcorcuera@gmail.com>
2019-02-12 09:42:10 -0500

    Specs for charges views as expected in seeds

--- source/spec/views/charges/index.html.erb_spec.rb
+++ source/spec/views/charges/index.html.erb_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "charges/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    Rails.application.load_seed
+    assign(:successful_charges, Charge.successful)
+    assign(:failed_charges, Charge.failed)
+    assign(:disputed_charges, Charge.disputed)
+    render
+  end
+
+  it 'should list 10 successful charges' do
+    expect(rendered).to have_css('.successful.charges tbody tr', count: 10)
+  end
+
+  it 'should list 5 failed charges' do
+    expect(rendered).to have_css('.failed.charges tbody tr', count: 5)
+  end
+
+  it 'should list 5 disputed charges' do
+    expect(rendered).to have_css('.disputed.charges tbody tr', count: 5)
+  end
+
 end
