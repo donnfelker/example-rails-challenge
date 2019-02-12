73c0264b4b82c91fecdaaf1dabf316efc9b7048d
Jose Corcuera <jzcorcuera@gmail.com>
2019-02-12 08:37:08 -0500

    Add Customer model

---
+++ source/app/models/customer.rb
@@ -0,0 +1,6 @@
+class Customer < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+end
---
+++ source/spec/models/customer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+
+  describe "required attributes" do
+
+    let(:customer) { Customer.new(first_name: 'Jose', last_name: 'Corcuera') }
+
+    it 'requires first_name' do
+      customer.first_name = nil
+      expect(customer.valid?).to be false
+      expect(customer.errors[:first_name]).to be_present
+    end
+
+    it 'requires last_name' do
+      customer.last_name = nil
+      expect(customer.valid?).to be false
+      expect(customer.errors[:last_name]).to be_present
+    end
+
+  end
+
+end
