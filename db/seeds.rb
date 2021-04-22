ed97af88d57a1738d1deddc632a9789026de32f3
RomanK2112 <roman.koshynskyi@gmail.com>
2021-04-22 05:15:49 +0300

    Implement seeds for customers with transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def transaction_params
+  {
+    created: Time.now.to_i,
+    amount: Random.rand(100..9_900),
+    currency: 'usd'
+  }
+end
+
+Customer.create(first_name: 'Johny', last_name: 'Flow')
+Customer.create(first_name: 'Raj', last_name: 'Jamnis')
+Customer.create(first_name: 'Andrew', last_name: 'Chung')
+Customer.create(first_name: 'Mike', last_name: 'Smith')
+
+# successful
+5.times { Customer.first.transactions << Transaction.successful.create(transaction_params) }
+3.times { Customer.second.transactions << Transaction.successful.create(transaction_params) }
+Customer.third.transactions << Transaction.successful.create(transaction_params)
+Customer.fourth.transactions << Transaction.successful.create(transaction_params)
+
+# failed
+3.times { Customer.third.transactions << Transaction.failed.create(transaction_params) }
+2.times { Customer.fourth.transactions << Transaction.failed.create(transaction_params) }
+
+# disputed
+3.times { Customer.first.transactions << Transaction.disputed.create(transaction_params) }
+2.times { Customer.second.transactions << Transaction.disputed.create(transaction_params) }
